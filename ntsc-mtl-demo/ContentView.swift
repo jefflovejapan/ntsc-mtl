630a679c0270bc17bcf5915eea0ed79e7ceb8d8a
Jeff Blagdon <jeffblagdon@gmail.com>
2024-05-23 09:18:26 -0400

    Initial Commit

---
+++ ntsc-mtl-demo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ntsc-mtl-demo
+//
+//  Created by Jeffrey Blagdon on 2024-05-23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ntsc-mtl-demo/ntsc_mtl_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ntsc_mtl_demoApp.swift
+//  ntsc-mtl-demo
+//
+//  Created by Jeffrey Blagdon on 2024-05-23.
+//
+
+import SwiftUI
+
+@main
+struct ntsc_mtl_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
