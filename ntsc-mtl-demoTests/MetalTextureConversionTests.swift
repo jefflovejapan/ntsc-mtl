4dc3f54f374a84ee42d9040b79b712fb5a70f93f
Jeff Blagdon <jeffblagdon@gmail.com>
2024-06-04 22:15:08 -0400

    More tests

--- ntsc-mtl-demoTests/MetalTextureConversionTests.swift
+++ ntsc-mtl-demoTests/MetalTextureConversionTests.swift
@@ -171,4 +171,38 @@ final class MetalTextureConversionTests: XCTestCase {
             try assertRoundTripWorks(color, message: "Mixmatch for color \(color)")
         }
     }
+    
+    func testIFullButterworthChromaLowpass() throws {
+        let device = try XCTUnwrap(device)
+        let texture = try XCTUnwrap(texture)
+        let commandQueue = try XCTUnwrap(commandQueue)
+        let library = try XCTUnwrap(library)
+        let commandBuffer = try XCTUnwrap(commandQueue.makeCommandBuffer())
+        var yiqa = [0.5, 0.5, 0.5, 1]
+        let region = MTLRegionMake2D(0, 0, 1, 1)
+        let bytesPerRow: Int = MemoryLayout<Float>.size * 4 * 1
+        texture.replace(region: region, mipmapLevel: 0, withBytes: &yiqa, bytesPerRow: bytesPerRow)
+        let iFunction = IIRTransferFunction.butterworth(cutoff: 1_300_000, rate: NTSC.rate * 1)
+        XCTAssertEqual(iFunction.numerators, [0.0572976321, 0.114595264, 0.0572976321])
+        XCTAssertEqual(iFunction.denominators, [1, -1.218135, 0.447325468])
+                                              
+        let iFilter = IIRTextureFilter(device: device, library: library, numerators: iFunction.numerators, denominators: iFunction.denominators, initialCondition: .zero, channel: .i, scale: 1, delay: 2)
+        try iFilter.run(outputTexture: texture, commandBuffer: commandBuffer)
+        commandBuffer.commit()
+        commandBuffer.waitUntilCompleted()
+        var z0: [Float] = Array(repeating: 0, count: 4)
+        var z1: [Float] = Array(repeating: 0, count: 4)
+        var z2: [Float] = Array(repeating: 0, count: 4)
+        
+        // Sampled from Swift, no idea if this is right
+        iFilter.zTextures[0].getBytes(&z0, bytesPerRow: bytesPerRow, from: region, mipmapLevel: 0)
+        XCTAssertEqual(z0, [0.0, 0.32268515, 0.0, 1.0])
+        iFilter.zTextures[1].getBytes(&z1, bytesPerRow: bytesPerRow, from: region, mipmapLevel: 0)
+        XCTAssertEqual(z1, [0.0, 0.055417147, 0.0, 1.0])
+        iFilter.zTextures[2].getBytes(&z2, bytesPerRow: bytesPerRow, from: region, mipmapLevel: 0)
+        XCTAssertEqual(z2, [0.0, 0.0, 0.0, 1.0])
+        let iValuesInZ = [z0, z1, z2].map({ $0[1] })
+        let expectedIValuesInZ: [Float] = [2.74764766E-9, 4.71874206E-10, 0]
+        XCTAssertEqual(iValuesInZ, expectedIValuesInZ)
+    }
 }
