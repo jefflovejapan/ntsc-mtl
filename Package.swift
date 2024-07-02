64e9e555da29bbd7f9eed8657582cbc292f23c7a
Jeff Blagdon <jeffblagdon@gmail.com>
2024-07-02 12:02:29 -0400

    Add manifest etc.

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "ntsc-mtl",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "ntsc-mtl",
+            targets: ["ntsc-mtl"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "ntsc-mtl"),
+        .testTarget(
+            name: "ntsc-mtlTests",
+            dependencies: ["ntsc-mtl"]),
+    ]
+)
